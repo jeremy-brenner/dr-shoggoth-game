797618c7f920d2e4401b2c6283990b5d12ce7948
Jeremy Brenner <jeremy.brenner@asynchrony.com>
2019-07-19 11:06:35 -0500

    simplify enum with lambdas

--- core/src/net/drshoggoth/games/drshoggoth/SceneResponse.kt
+++ core/src/net/drshoggoth/games/drshoggoth/SceneResponse.kt
@@ -5,36 +5,22 @@ import net.drshoggoth.games.drshoggoth.scenes.GameScene
 import net.drshoggoth.games.drshoggoth.scenes.PauseScene
 import net.drshoggoth.games.drshoggoth.scenes.TitleScene
 
-enum class SceneResponse {
-    NONE {
-        override fun execResponse() {}
-    },
-    DONE_LOADING {
-        override fun execResponse() {
-            SceneManager.current = listOf(TitleScene)
-        }
-    },
-    NEW_GAME {
-        override fun execResponse() {
-            GameScene.newGame()
-            SceneManager.current = listOf(BackgroundScene, GameScene)
-        }
-    },
-    GAME_OVER {
-        override fun execResponse() {
-            SceneManager.current = listOf(TitleScene)
-        }
-    },
-    PAUSE {
-        override fun execResponse() {
-            SceneManager.current = listOf(BackgroundScene, PauseScene)
-        }
-    },
-    UNPAUSE {
-        override fun execResponse() {
-            SceneManager.current = listOf(BackgroundScene, GameScene)
-        }
-    };
-
-    abstract fun execResponse()
+enum class SceneResponse(val response: () -> Unit) {
+    NONE({}),
+    DONE_LOADING({
+        SceneManager.current = listOf(TitleScene)
+    }),
+    NEW_GAME({
+        GameScene.newGame()
+        SceneManager.current = listOf(BackgroundScene, GameScene)
+    }),
+    GAME_OVER({
+        SceneManager.current = listOf(TitleScene)
+    }),
+    PAUSE({
+        SceneManager.current = listOf(BackgroundScene, PauseScene)
+    }),
+    UNPAUSE({
+        SceneManager.current = listOf(BackgroundScene, GameScene)
+    });
 }
